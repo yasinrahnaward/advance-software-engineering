9476f4b8221dea9163f06fddb76e683a9145db8e
unknown <yasinrahnaward.2022@gmail.com>
2022-09-09 23:09:18 +0430

    stragey patter implement

--- strategy pattern/app.js
+++ strategy pattern/app.js
@@ -1,1 +1,62 @@
-console.log("helloo yasmin");
+// interface
+// // class
+var nofly = /** @class */ (function () {
+    function nofly() {
+    }
+    nofly.prototype.fly = function () {
+        console.log("I can not fly");
+    };
+    return nofly;
+}());
+var fly = /** @class */ (function () {
+    function fly() {
+    }
+    fly.prototype.fly = function () {
+        console.log(" I can fly");
+    };
+    return fly;
+}());
+var quick = /** @class */ (function () {
+    function quick() {
+    }
+    quick.prototype.quick = function () {
+        console.log(" I can quick");
+    };
+    return quick;
+}());
+var noquick = /** @class */ (function () {
+    function noquick() {
+    }
+    noquick.prototype.quick = function () {
+        console.log("I have not quick");
+    };
+    return noquick;
+}());
+// injection
+var duckbehavior = /** @class */ (function () {
+    function duckbehavior() {
+    }
+    duckbehavior.prototype.setfly = function (fly) {
+        this.flyable = fly;
+    };
+    duckbehavior.prototype.setquick = function (quick) {
+        this.quickable = quick;
+    };
+    duckbehavior.prototype.performfly = function () {
+        this.flyable.fly();
+    };
+    duckbehavior.prototype.performquick = function () {
+        this.quickable.quick();
+    };
+    return duckbehavior;
+}());
+var duckone = new duckbehavior();
+duckone.setfly(new fly());
+duckone.setquick(new noquick());
+duckone.performfly();
+duckone.performquick();
+var ducktwo = new duckbehavior();
+ducktwo.setfly(new nofly());
+ducktwo.setquick(new quick());
+ducktwo.performfly();
+ducktwo.performquick();
--- strategy pattern/app.ts
+++ strategy pattern/app.ts
@@ -1,5 +1,61 @@
-function fedex(){
-    this.calculate=package=>{
-        
-    }
+// interface
+
+interface Fly {
+  fly(): void;
 }
+
+interface Quick {
+  quick(): void;
+}
+// // class
+class nofly implements Fly {
+  fly(): void {
+    console.log("I can not fly");
+  }
+}
+
+class fly implements Fly {
+  fly(): void {
+    console.log(" I can fly");
+  }
+}
+class quick implements Quick {
+  quick(): void {
+    console.log(" I can quick");
+  }
+}
+class noquick implements Quick {
+  quick(): void {
+    console.log("I have not quick");
+  }
+}
+// injection
+class duckbehavior {
+  flyable: Fly;
+  quickable: Quick;
+
+  setfly(fly: Fly): void {
+    this.flyable = fly;
+  }
+
+  setquick(quick: Quick): void {
+    this.quickable = quick;
+  }
+  performfly(): void {
+    this.flyable.fly();
+  }
+  performquick(): void {
+    this.quickable.quick();
+  }
+}
+
+let duckone = new duckbehavior();
+duckone.setfly(new fly());
+duckone.setquick(new noquick());
+duckone.performfly();
+duckone.performquick();
+let ducktwo = new duckbehavior();
+ducktwo.setfly(new nofly());
+ducktwo.setquick(new quick());
+ducktwo.performfly();
+ducktwo.performquick();
